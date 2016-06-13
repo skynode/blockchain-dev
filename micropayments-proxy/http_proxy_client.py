635fb578d32711f4ca4489d4f0c9b0c03383ecc6
James Valkyrie <jim.valkyrie@gmx.ch>
2016-06-13 06:34:53 +0100

    began work on the HTTP proxy client that will eventually outsource payment functions to a proxy server

--- micropayments-proxy/http_proxy_client.py
+++ micropayments-proxy/http_proxy_client.py
@@ -1,24 +1,37 @@
 '''
+This client will invoke the micropayments-proxy, payments_handler.py,
+only when the client receives an HTTP status_code 402 from some 
+service running on some server.
+The payments-handler won't come into play under any other scenario.
 
-
+---MIT Licensed
 '''
 
 #!/usr/bin/env python3
 
-#do we need these import statements?
-from two1.wallet import Wallet
-from two1.bitrequests import BitTransferRequests
+import requests
 
-HTTP_SERVER  = "http://127.0.0.1:5000"
-PROXY_SERVER = "http://127.0.0.1:9000"
+VANILLA_SERVER      = "http://127.0.0.1:5050"
+PAYWALLED_SERVER    = "http://127.0.0.1:5051
+PAYMENTS_PROXY      = "http://127.0.0.1:9000"
 
 proxies = {"http": PROXY_SERVER}
 
-def main():
+def main(url):
+        #take url parameter from stdin
+        input('please enter the resource locator here: ')
+        #include the validated url in the GET function 
+        try:
+            if(requests.get(url).status_code != 402):
         
+        #response from VANILLA_SERVER
+        print ("You've hit the VANILLA_SERVER!".upper())
+        print (requests.get(url=VANILLA_SERVER).text)
+        
+        print ()               
         
         
         
         #test that payments-handler server is up
-        #if response.status_code=50x, don't pay... 
+        #if response.status_code=50x, return... 
         #close connection with satoshi-requesting server
--- micropayments-proxy/vanilla_http_server.py
+++ micropayments-proxy/vanilla_http_server.py
@@ -2,8 +2,9 @@
 
 from flask import Flask, request
 
-app = Flask(__name__)
+app = -Flask(__name__)
 
+#for requests to the root hierarchy, apply the 'hello' function
 @app.route("/")
 def hello():
      return "hello client!\n
@@ -11,20 +12,22 @@ def hello():
 
 #if response.status_code=402 (payment required), 
 #then route response thru paymnts-handler-proxy
-'''@app.route("/proxyclient")
+'''
+@app.route("/proxyclient")
 def confirmproxy():
     if "X-Forwarded-For" in request.headers:
         return "you are behind a proxy. check\n"
     else:
         return "no proxy"
-        '''
+'''
  
  #if we hit a satoshi-friendly paywall
  #bearing the HTTP 402 status code
  #route traffic to payments-handler
+ '''
  @app.route("/paywall")
  def activatepaymentshandler():
         
-
+'''
 if __name__ == "__main__":
-    app.run()
+    app.run(host='127.0.0.1', port=5050)
