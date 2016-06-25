acd6be57477a306e38a43adb28036a2c65acf2ad
James Valkyrie <jim.valkyrie@gmx.ch>
2016-06-26 00:49:32 +0100

    changed from python requests to 21.co BitTransferRequests

--- micropayments-proxy/payments_handler.py
+++ micropayments-proxy/payments_handler.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
 
-import requests as pyreq
-
 from flask import Flask, request, Response
 from werkzeug.datastructures import Headers
 
@@ -10,66 +8,41 @@ from two1.wallet import Wallet
 #originally handled by the requesting client 
 #Now outsourced to payment_handler.py
 from two1.bitrequests import BitTransferRequests
+from two1.bitrequests import BitRequestsError
 
 app = Flask(__name__)
 
 #instantiate a wallet
 _wallet = Wallet()
 
-#this function handles bitcoin payments using the wallet object as parameter
-btc_reqs = BitTransferRequests(_wallet)
+#this function handles bitcoin micropayments using the wallet object 
+btc_requests = BitTransferRequests(_wallet)
 
 ''' simple satoshi-payment proxy with one function:
     receive request from client and process
     payment on destination server address 
     passed as url parameter in proxy client GET request
 '''
-@app.route('/<path:url>')
-def main(url):
+@app.route('/')
+def main():
     try:        
-        #manipulate HTTP header information
-        h = Headers(request.headers)
-        h.add('X-Forwarded-For', request.remote_addr) #potentially prone to IP spoofing
-        
-        #would work with a proxy server that has the @payment.required() descriptor
-        '''
-        h.remove('HTTP_BITCOIN_MICROPAYMENT_SERVER')
-        h.remove('HTTP_RETURN_WALLET_ADDRESS')
-        h.remove('Bitcoin-Transfer')
-        h.remove('Authorization')
-        h.remove('Content-Length')
-        '''
-        
         #create a response object to hold request result based on param url received from client
-        res = pyreq.request(
+        res = btc_requests.request(
             method=request.method, 
-            url=request.url,
-            headers=h,
-            files=request.files if request.files else None,
+            url=request.url,            
             data=request.data if request.data else None,
+            max_price=None,
             params=request.args if request.args else None,
             timeout=10
-        )
-    except(pyreq.exceptions.Timeout, 
-        pyreq.exception.ConnectTimeout,
-        pyreq.exception.ReadTimeout
-    ):
-        return Response(status=504)
-    except(pyreq.exceptions.ConnectionError, 
-        pyreq.exceptions.HTTPError,
-        pyreq.exceptions.TooManyRedirects
-    ):
-        return Response(status=502)
-    except(pyreq.exceptions.RequestException, Exception) as e:
-        if app.debug:
-           raise e
-        return Response(status=500)
+        )            
+
+    except BitRequestsError as biterror:
+        print("BitRequestsError: {0}".format(biterror))
      
-    headers = list(res.headers.items())
-    return Response(res.text if res.text else "internal server error", 
-        status=res.status_code,
-        headers=headers
-     )
+    #return response from micropayments proxy to client
+    return Response(res.text if res.text else "remote server error", 
+        status=res.status_code        
+    )
      
 if __name__=='__main__':
     app.run(host='0.0.0.0',port=9000)
